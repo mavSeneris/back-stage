d47eecd14edaa1376a33def0079874677ddd86b0
Maverick Seneris <mavseneris@gmail.com>
2023-01-18 14:38:45 +0800

    added main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='main'>
+      <div className='main-wrapper'>
+        <img className='cover' src='https://backstagetalks.com/img/backstagetalks_cover_issue_6.png' />
+        <div className='main-cover-text'>
+          <span className='issue-num'>Issue #5</span>
+          <span className='buy-here'><a href='#'>Buy here</a></span>
+          <span className=''>or in <a href='#'>selected stores.</a></span>
+        </div>
+
+
+      </div>
+
+      <div className='main-wrapper'>
+        <img className='cover' src='https://backstagetalks.com/img/backstagetalks_cover_issue_5.png' />
+        <div className='main-cover-text'>
+          <span className='issue-num'>Issue #5</span>
+          <span className='buy-here'><a href='#'>Buy here</a></span>
+          <span className=''>or in <a href='#'>selected stores.</a></span>
+        </div>
+
+
+      </div>
+
+      <div className='main-wrapper'>
+        <img className='cover' src='https://backstagetalks.com/img/backstagetalks_cover_issue_4.png' />
+        <div className='main-cover-text'>
+          <span className='issue-num'>Issue #5</span>
+          <span className='buy-here'><a href='#'>Buy here</a></span>
+          <span className=''>or in <a href='#'>selected stores.</a></span>
+        </div>
+
+
+      </div>
+
+    </div>
+  )
+}
