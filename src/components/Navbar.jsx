3f32ff6dad82df432d2b28fb16ee6f4035f0a1e7
Maverick Seneris <mavseneris@gmail.com>
2023-01-18 13:48:58 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      {/* <h1 className='logo'>Backstage Talks</h1> */}
+      <img  className='logo' src='https://backstagetalks.com/img/logo.png'/>
+      <span className='contact'>info@backstagetalks.com</span>
+    </div>
+  )
+}
